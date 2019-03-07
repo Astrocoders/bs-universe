3250576ca9307e404824e348666aafede2a08a0e
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-07 13:39:49 -0300

    feat: create universe.json before at each commit

---
+++ generateData.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+console.log("Generating universe.json, please wait...")
+
+const files = fs.readdirSync('.')
+const universe = []
+
+for (let file of files) {
+  if (fs.existsSync(`${file}/package.json`)) {
+    const data = JSON.parse(fs.readFileSync(`${file}/package.json`))
+    universe.push({
+      title: data.name,
+      description: data.description,
+      url: `https://github.com/Astrocoders/bs-universe/tree/master/${file}`
+    })
+  }
+}
+
+fs.writeFileSync('universe.json', JSON.stringify(universe, null, 2), 'utf-8')

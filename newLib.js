c4ac9c007a22e02f344cad322115a7b92cd3980e
jrquadros <jrmquadros@gmail.com>
2019-09-06 16:23:36 -0300

    feature(scripts): add react package script

--- newLib.js
+++ newLib.js
@@ -5,13 +5,15 @@ const projectName = process.argv.slice(2).toString()
 
 const execAsync = util.promisify(cp.exec)
 
+const isReact = process.env.IS_REACT ? 'react' : 'lib'
+
 async function execute(command) {
   const logs = await execAsync(command)
   process.stdout.write(logs.stdout)
 }
 
 const main = async () => {
-  await execute(`npx hygen lib new ${projectName}`)
+  await execute(`npx hygen ${isReact} new ${projectName}`)
   await execute(`npx lerna create ${projectName}  --yes`)
 }
 

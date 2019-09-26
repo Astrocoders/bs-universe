b7ef347251299629af973038b09cc9621deb6593
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2019-09-26 18:41:14 -0300

    fix(generate-data): fix script to match the new project structure

--- generateData.js
+++ generateData.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 
 console.log("Generating universe.json, please wait...")
 
-const universe = fs.readdirSync('.').map(file => {
-  if(fs.existsSync(`${file}/package.json`)) {
-    const { name: title, description } = JSON.parse(fs.readFileSync(`${file}/package.json`))
-    return { title, description, url: `https://github.com/Astrocoders/bs-universe/tree/master/packages/${file}` }
+const universe = fs.readdirSync('./packages').map(dir => {
+  if(fs.existsSync(`packages/${dir}/package.json`)) {
+    const { name: title, description } = JSON.parse(fs.readFileSync(`packages/${dir}/package.json`))
+    return { title, description, url: `https://github.com/Astrocoders/bs-universe/tree/master/packages/${dir}` }
   }
   return null
 }).filter(Boolean)
